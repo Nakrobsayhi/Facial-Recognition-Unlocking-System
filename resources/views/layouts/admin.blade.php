4d0aa102a78759cffc5a5fff216157b12b5c439c
Nakrobsayhi <n4kr0bs4yh1@gmail.com>
2026-02-02 18:04:11 +0700

    2/2 18:00

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -99,18 +99,16 @@
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    <!-- <form method="GET" action="{{ route('admin.user.index') }} class=" d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small"
-                                placeholder="ค้นหาชื่อผู้ใช้" aria-label="Search" aria-describedby="basic-addon2">
+                            <input type="text" name="search" class="form-control bg-light border-0 small" placeholder="ค้นหาชื่อผู้ใช้" aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="button">
                                     <i class="fas fa-search fa-sm"></i>
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> -->
 
                     <ul class="navbar-nav ml-auto">
 
