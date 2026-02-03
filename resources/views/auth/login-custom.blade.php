11e4ebb83e0da47244d9bf61b0c62286c9474cfe
Nakrobsayhi <n4kr0bs4yh1@gmail.com>
2026-02-03 20:53:13 +0700

    welcome

--- resources/views/auth/login-custom.blade.php
+++ resources/views/auth/login-custom.blade.php
@@ -31,7 +31,7 @@
                             <div class="col-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">ยินดีต้อนรับ!</h1>
                                     </div>
 
                                     <form class="user" action="{{ route('login') }}" method="post">
